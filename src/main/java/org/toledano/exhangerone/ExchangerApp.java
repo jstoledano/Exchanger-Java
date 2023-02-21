09b9008285e32929f710dd4366bcd3ccfb8003a9
Javier Sanchez Toledano <jstoledano@users.noreply.github.com>
2023-02-20 22:19:34 -0600

    Funciona el controller

--- src/main/java/org/toledano/exhangerone/ExchangerApp.java
+++ src/main/java/org/toledano/exhangerone/ExchangerApp.java
@@ -1,20 +1,25 @@
 package org.toledano.exhangerone;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class ExchangerApp extends Application {
+
     @Override
-    public void start(Stage primaryStage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(ExchangerApp.class.getResource("first-one.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 640, 480);
-        primaryStage.setTitle("Exchanger ONE");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(ExchangerApp.class.getResource("dialogOne.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 240);
+        stage.setTitle("Exchanger ONE");
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void main(String[] args) {
