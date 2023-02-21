c65b518ba97578ba2309407919123735814923d4
Javier Sanchez Toledano <jstoledano@users.noreply.github.com>
2023-02-20 22:53:47 -0600

    Funciona la selección inicial.

--- src/main/java/org/toledano/exhangerone/controllerOne.java
+++ src/main/java/org/toledano/exhangerone/controllerOne.java
@@ -3,20 +3,26 @@ package org.toledano.exhangerone;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.Label;
+
 
 public class controllerOne {
     @FXML private ChoiceBox<String> cb;
+    @FXML private Button btn;
 
     ObservableList<String> cbOptions = FXCollections
             .observableArrayList(
-                    "Conversor de moneda",
-                    "Conversor de temperatura"
+                    "Convertidor de moneda",
+                    "Convertidor de temperatura"
             );
 
     @FXML private void initialize() {
         cb.setItems(cbOptions);
-        cb.setValue("Conversor de moneda");
+        cb.setValue("Convertidor de moneda");
+    }
+
+    @FXML private void selection() {
+        System.out.println(cb.getValue());
     }
 }
